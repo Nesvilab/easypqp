3d6de3c1902ef7828a84379e537b123550e5ee3d
Dan Polasky <dpolasky@umich.edu>
2024-08-06 10:22:09 -0400

    add library test command

--- tests/test_command.py
+++ tests/test_command.py
@@ -2,11 +2,11 @@
 import click
 import sys
 
-from easypqp.main import convertpsm
-from easypqp.main import convertpsm_core
+from easypqp.main import convertpsm, convertpsm_core
+from easypqp.main import library_core, library
 
 
-def parse_arguments(argument_string):
+def parse_arguments_convertpsm(argument_string):
     """
     Parse the input string arguments into a dictionary of parameters.
     """
@@ -15,6 +15,24 @@ def parse_arguments(argument_string):
     return ctx.params
 
 
+def parse_arguments_library(argument_string):
+    """
+    Parse the input string arguments into a dictionary of parameters.
+    """
+    ctx = click.Context(library)
+    opts = ctx.command.parse_args(ctx, argument_string)
+    return ctx.params
+
+
 if __name__ == '__main__':
-    cmdargs = parse_arguments(sys.argv[1:])
-    convertpsm_core(**cmdargs)
+    if sys.argv[1] == 'convertpsm':
+        cmdargs = parse_arguments_convertpsm(sys.argv[2:])
+        convertpsm_core(**cmdargs)
+    elif sys.argv[1] == 'library':
+        cmdargs = parse_arguments_library(sys.argv[2:])
+        library_core(**cmdargs)
+    else:
+        print('invalid command string or unsupported command for this test')
+
+    print('done')
+    sys.exit(0)
